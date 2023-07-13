f0275ce753c540b831a6f8561621057da08e124f
GMKOO <karenmm@naver.com>
2023-07-13 10:32:48 +0900

    스프링 세번째 수업 및 깃 업로드 연습

---
+++ jul13/src/main/java/com/karenmm/web/BoardController.java
@@ -0,0 +1,20 @@
+package com.karenmm.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BoardController {
+	//Ctrl + shift + o  임포트 정리
+	//사용자가 /board라고  호출하면 동작할 메소드를 만들겠습니다.
+	
+	@GetMapping("/board")
+	public String board() {
+		return "board";  // jsp파일명 
+	}
+	
+//	@PostMapping("/board")
+	
+
+}
